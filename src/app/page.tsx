20013ce6d6a57057736b8ddde246a7a61e25e740
Andrew Akyampong <andrewakyampong@gmail.com>
2025-01-06 00:06:08 +0000

    colors reverted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Navbar } from '@/components/custom/navbar';
 
 export default function Home() {
   return (
-    <div className='flex min-h-screen flex-col bg-[#001810]'>
+    <div className='flex min-h-screen flex-col bg-muted'>
       <Navbar />
       <main className='container mx-auto py-10'>
         <TokenAnalysisForm />
